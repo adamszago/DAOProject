051080d2398cee579201d7d4f09a6c8dce206fee
Adams Zago <adams.zago@gmail.com>
2019-05-14 10:34:18 -0300

    Entity class Department created

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package src.application;
+
+import src.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department dpt = new Department(1, "book");
+		System.out.println(dpt);
+	}
+
+}
