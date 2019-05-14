ce815cb02726f5fe723452c720e64d45a98fd157
Adams Zago <adams.zago@gmail.com>
2019-05-14 10:57:04 -0300

    Created classes SellerDaoJDBC and DAOFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package src.model.dao.impl;
+
+import java.util.List;
+
+import src.entities.Seller;
+import src.model.dao.SellerDAO;
+
+public class SellerDaoJDBC implements SellerDAO {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deletById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
